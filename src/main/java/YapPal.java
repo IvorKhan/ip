ee6e2dd1f035f9fc021d8d929e17c9de86fc5949
Kho Wei Fong <navil.nanhua@gmail.com>
2025-08-18 10:51:56 +0800

    Added echo functionality

--- src/main/java/YapPal.java
+++ src/main/java/YapPal.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+
 public class YapPal {
-    // public state
-    public static final String botName = "YapPal";
-    public static final String introMsg =
-        "Hello! I'm " + botName + "\n" +
+    // chatbot constants
+    public static final String BOT_NAME = "YapPal";
+    public static final String INTRO_MSG =
+        "Hello! I'm " + BOT_NAME + "\n" +
         "What can I do for you?";
-    public static final String goodbyeMsg =
+    public static final String GOODBYE_MSG =
         "Hope to see you again soon!";
+    public static Scanner scanner = new Scanner(System.in);
+
+    // state constants
+    private static final int TERMINATE = -1;
+    private static final int LISTENING = 0;
 
     public static void main(String[] args) {
-        YapPal.printMsg(introMsg);
-        YapPal.printMsg(goodbyeMsg);
+        YapPal.printMsg(YapPal.INTRO_MSG);
+        int state = YapPal.LISTENING;
+        while (state != YapPal.TERMINATE) {
+            state = YapPal.listen();
+        }
+        YapPal.printMsg(YapPal.GOODBYE_MSG);
+    }
+
+    public static int listen() {
+        String command = YapPal.scanner.nextLine();
+        if (command.equals("bye")) {
+            return YapPal.TERMINATE;
+        }
+        YapPal.printMsg(command);
+        return YapPal.LISTENING;
     }
 
     public static void printMsg(String msg) {
